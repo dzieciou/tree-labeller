71d5d2f3ee755f5430b2c08ca282e84d8a43a5d1
Paweł Kubiak <pawel.kubiak@protonmail.com>
2023-01-19 01:52:49 +0100

    feat: many improvements

--- web-tagger/main.py
+++ web-tagger/main.py
@@ -3,6 +3,7 @@ import os, csv, json
 from js import window, document, Blob
 from pyodide.http import pyfetch
 from pyodide import create_proxy
+from pyodide.ffi.wrappers import set_timeout
 
 from tree_labeller.create_task import create_task
 from tree_labeller.label import label
@@ -12,6 +13,25 @@ TASKS = None
 ALREADY_DONE_TASKS = None
 ITERATION = 1
 INDEX = 0
+
+def qs(selector):
+    return document.querySelector(selector)
+
+def el(tag, id=None, class_=None, text=None):
+    e = document.createElement(tag)
+    if id: e.id = e
+    if class_: e.className = class_
+    if text: e.innerText = text
+    return e
+
+
+def display_progress(text):
+    qs('#loadingLabel').innerText = text
+    qs('#loadingSpinner').classList.remove('d-none')
+
+def hide_progress():
+    qs('#loadingSpinner').classList.add('d-none')
+
 
 def save_results():
     global ITERATION
@@ -26,6 +46,7 @@ def save_results():
     ITERATION += 1
     load_tasks()
 
+
 def set_task_label(label):
     global INDEX
     TASKS[INDEX]['label'] = label
@@ -36,6 +57,7 @@ def set_task_label(label):
     else:
         save_results()
 
+
 def set_task_label_fn(event):
     set_task_label(event.target.value)
 
@@ -43,64 +65,113 @@ set_task_label_proxy = create_proxy(set_task_label_fn)
 
 
 def create_label_button(label, value, className):
-    btn = document.createElement('button')
-    btn.innerText = label
+    btn = el('button', text=label, class_=f"{className} m-1")
     btn.value = value
-    btn.className = className + ' m-1'
     btn.addEventListener("click", set_task_label_proxy)
-    document.getElementById('actions').appendChild(btn)
+    return btn
 
 
 def display_task():
     task = TASKS[INDEX]
-    document.getElementById('title').innerText = f"Task {INDEX+1} / {len(TASKS)}; Iteration #{ITERATION}"
-    document.getElementById('task').classList.remove('d-none')
-    document.getElementById('progress').classList.remove('d-none')
+    qs('#taskForm').classList.remove('d-none')
+    qs('#stats_progress_Task').innerText = f"{INDEX+1} / {len(TASKS)}"
+    qs('#taskName').innerText = task['name']
+    qs('#taskCategory').innerText = task['category'].replace(">", " » ")
 
-    params = ''
-    
+    # Display Task Params
+    params = ""
     for k, v in task.items():
-        # if k == 'label': continue
-        params += f"<li>{k}: {v}</li>"
-    document.getElementById('params').innerHTML = params
+        if k in ('label', 'category', 'name', 'id'): continue
+        params += f'<li class="list-group-item text-muted">{k}: {v}</li>'
+    qs('#taskParams').innerHTML = params
 
-    if task['label'] == '':
-        labels = LABELS
-    else:
-        labels = task['label'].split('|')
-    
-    if len(labels) == 1:
-        return set_task_label(labels[0])
+    # # Display Task Buttons
+    predicted = set(task['label'].split('|'))
+    predicted.discard('')
+    other = (set(LABELS)|{'?','!'}) - predicted
+    print(">>>>>>>>>>", predicted, other)
 
-    document.getElementById('actions').innerHTML = ''
-    for label in labels:
-        create_label_button(label, label, 'btn btn-success')
-    
-    create_label_button('Skip', '?', 'btn btn-warning')
-    create_label_button('Reject', '!', 'btn btn-warning')
+    qs('#taskButtons').innerHTML = ""
+    for title, labels in [("Predicted", sorted(predicted)), ("Other", sorted(other))]:
+        e = el("div", class_="text-center mb-3 mt-4")
+        e.appendChild(el("h3", class_="d-inline me-2 align-middle", text=f"{title}:"))
+        for value in labels:
+            cls = 'primary' if title == 'Predicted' else 'secondary'
+            if value == '?':
+                label = 'Skip'
+                cls = 'warning'
+            elif value == '!':
+                label = 'Reject'
+                cls = 'warning'
+            else:
+                label = value.capitalize()
+
+            btn = create_label_button(label, value, f'btn btn-{cls}' + ('' if title == 'Predicted' else ' btn-sm'))
+            e.appendChild(btn)
+
+        qs('#taskButtons').appendChild(e)
+
+
+def set_statistics(stats):
+    container = document.getElementById('statistics')
+    container.innerHTML = ""
+
+    for name, props in stats.items():
+        h5 = document.createElement('h5')
+        h5.innerText = name
+        h5.className = 'text-light'
+        container.appendChild(h5)
+        dl = document.createElement('dl')
+        dl.className = "row opacity-50 mb-3"
+        html = ''
+        for k, v in props.items():
+            html += f'<dt class="col-sm-8">{k}:</dt><dd class="col-sm-4" id="stats_{name}_{k}">{v}</dd>'
+        dl.innerHTML = html
+        container.appendChild(dl)
 
 
 def check_progress():
     with open(f"output/{ITERATION}-stats.json") as file:
         stats = json.load(file)
-        progress = stats['progress.py']
-        output = '; '.join(f"<b>{k}:</b> {round(v,2) if isinstance(v,float) else v}" for k, v in progress.items())
-        document.getElementById('progress').innerHTML = output
-
         print(stats)
+
+        set_statistics({
+            "progress": {
+                "Iteration": ITERATION,
+                "Task": "∞"
+            },
+            "labels": {
+                "Provided": f"{stats['manual_labels']['n_allowed_labels']}",
+                "Missing": f"{100*stats['progress.py']['missing']:.1f}%",
+                "Covered": f"{100*stats['progress.py']['allowed_labels']:.1f}%",
+            },
+            "predictions": {
+                "Univocal labels": f"{100*stats['progress.py']['univocal']:.1f}%",
+                "Ambiguous labels": f"{100*stats['progress.py']['ambiguous']:.1f}%"
+            }
+        })
     
 
 def load_tasks():
     global TASKS, INDEX, ALREADY_DONE_TASKS
     INDEX = 0
     print(">> Loading labels from:", f"output/{ITERATION}-to-verify.tsv")
+    display_progress("Loading Tasks")
 
     label(dir="output/", sample=10)
     check_progress()
 
     if not os.path.exists(f"output/{ITERATION}-to-verify.tsv"):
-        document.getElementById('task').innerHTML = '<h1>You are done 👏</h1>'
+        hide_progress()
+        qs('#taskForm').classList.add('d-none')
+        qs('#successForm').classList.remove('d-none')
         return 
+
+    if os.path.exists(f"output/{ITERATION}-mapping.tsv"):
+        qs('#downloadMapping').disabled = ''
+    
+    if os.path.exists(f"output/{ITERATION}-good.tsv"):
+        qs('#downloadPredictedLabels').disabled = ''
 
     with open(f"output/{ITERATION}-to-verify.tsv") as file:
         tsv = csv.DictReader(file, delimiter="\t")
@@ -111,24 +182,40 @@ def load_tasks():
                 ALREADY_DONE_TASKS.append(record)
             else:
                 TASKS.append(record)
+
+    hide_progress()
     display_task()
 
 
-def download_mapping(event):
-    with open(f"output/{ITERATION}-mapping.tsv", encoding='utf-8') as file:
+def download_file(path, fname):
+    if not os.path.exists(path):
+        return 
+
+    with open(path, encoding='utf-8') as file:
         content = file.read()
 
     blob = Blob.new([content], {type : 'application/text'})
     url = window.URL.createObjectURL(blob) 
-    window.location.assign(url)
 
-download_mapping_proxy = create_proxy(download_mapping)
+    downloadLink = document.createElement("a");
+    downloadLink.href = url;
+    downloadLink.download = fname;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+
+
+def download_mapping(event):
+    download_file(f"output/{ITERATION}-mapping.tsv", "mapping.tsv")
+
+
+def download_predicted_labels(event):
+    download_file(f"output/{ITERATION}-good.tsv", "predicted-labels.tsv")
+
 
 async def create_task_fn(*args):
     global LABELS
-    print('hello', args)
     url = document.getElementById('input-url').value
-    LABELS = document.getElementById('input-labels').value.split(",")
+    LABELS = document.getElementById('input-labels').value.split("|")
 
     print(">>", url, LABELS)
 
@@ -138,11 +225,15 @@ async def create_task_fn(*args):
 
     print(os.getcwd(), os.path.exists("tree.yaml"), os.path.getsize("tree.yaml"))
 
-    create_task(dir="output/", tree="tree.yaml", allowed_labels=LABELS)
     document.getElementById('input-form').classList.add('d-none')
-    load_tasks()
+    display_progress("Creating Task")
 
-function_proxy = create_proxy(create_task_fn)
-document.getElementById("input-button").addEventListener("click", function_proxy)
-document.getElementById("download").addEventListener('click', download_mapping_proxy)
+    def task():
+        create_task(dir="output/", tree="tree.yaml", allowed_labels=LABELS)
+        set_timeout(load_tasks, 100)
+    
+    set_timeout(task, 100)
 
+qs("#createTaskBtn").addEventListener("click", create_proxy(create_task_fn))
+qs("#downloadMapping").addEventListener('click', create_proxy(download_mapping))
+qs("#downloadPredictedLabels").addEventListener('click', create_proxy(download_predicted_labels))
